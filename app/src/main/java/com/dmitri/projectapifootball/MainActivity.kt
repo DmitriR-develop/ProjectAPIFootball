112eec536ae19db6bb178eb4a2c476cda80ef61b
dmitri <dmitrivitr@gmail.com>
2021-06-30 11:22:16 +0300

    Initial commit

---
+++ app/src/main/java/com/dmitri/projectapifootball/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dmitri.projectapifootball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
